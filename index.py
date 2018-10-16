6d4211632d1b82dab4a5f7d3992d1e9e3338cf1e
Jack C <chen.jack@email.com>
2018-10-16 17:53:51 -0400

    Created move flag

--- index.py
+++ index.py
@@ -3,11 +3,30 @@ import sys
 import shutil
 
 SCRIPT_PARAMS = sys.argv[1:]
-
-SCRIPT_LOCATION = os.getcwd()
 HOME = os.getenv("HOME")
 
 BASE_DIRECTORIES = ["Desktop", "Documents", "Downloads", "Music", "Pictures", "Public", "Videos"]
+
+# If you clone the repo from github. The parent folder is visible. 
+# This function makes the folder hidden and reallocates it to Home.
+
+def hide():
+	path_to_dir = os.getcwd() # The path to the directory of containing this code
+	BASENAME = os.path.basename( path_to_dir ) # The name of the directory containing this code
+
+	print("Base", BASENAME)
+	if( not isHiddenFile(BASENAME) ):
+		print("Renaming Folder")
+		new_base_name = "." + BASENAME
+		new_name = os.path.abspath(os.path.join(os.pardir, new_base_name))
+		print("NEW NAME", new_name)
+
+def move():
+	SCRIPT_LOCATION = os.getcwd() # The path to the directory of containing this code
+	#BASENAME = os.path.basename( path_to_dir ) # The name of the directory containing this code
+	#print("Move", path_to_dir, BASENAME)
+	print("Moving", SCRIPT_LOCATION, "to", HOME)
+	shutil.move(SCRIPT_LOCATION, HOME)
 
 
 def isHiddenFile(filename):
@@ -38,8 +57,9 @@ def main():
 	# Removing all files within Home
 	print("Phase 2")
 	os.chdir(HOME)
-	for content in os.listdir(): # Iterate through contents of Home
 
+	# Iterate through contents of Home
+	for content in os.listdir():
 		# Don't delete hidden files within Home directory and dont delete the base directory
 		if( (not isHiddenFile(content)) and (content not in BASE_DIRECTORIES) ): 
 			path_of_content = os.path.join(HOME, content)
@@ -50,4 +70,13 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+	FLAG = None
+
+	if(len(SCRIPT_PARAMS) > 0 ):
+		FLAG = SCRIPT_PARAMS[0]
+
+	if(FLAG == "move"):
+		move()
+	else:
+		pass
+		#main()
