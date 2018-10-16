df0502d99c1d289424f5af952c0a098e2768ed0b
Jack C <chen.jack@email.com>
2018-10-16 18:46:02 -0400

    Script completed. Needs checking

--- index.py
+++ index.py
@@ -2,13 +2,21 @@ import os
 import sys
 import shutil
 
-SCRIPT_PARAMS = sys.argv[1:]
 HOME = os.getenv("HOME")
-
+SCRIPT_LOCATION = os.getcwd()
 BASE_DIRECTORIES = ["Desktop", "Documents", "Downloads", "Music", "Pictures", "Public", "Videos"]
 
-# If the parent folder of this script is visible, hide it.
+def isFolderVisible():
+	'''
+	Checks if the parent folder of this script is visible
+	'''
+	BASENAME = os.path.basename(os.getcwd())
+	return not isHiddenFile(BASENAME)
+
 def hide():
+	'''
+	If the parent folder of this script is visible, hide it.
+	'''
 	old_name = os.getcwd() # The path to the directory of containing this code
 	BASENAME = os.path.basename( old_name ) # The name of the directory containing this code
 
@@ -18,21 +26,17 @@ def hide():
 		new_name = os.path.abspath(os.path.join(os.pardir, new_base_name))
 		print(old_name, "to", new_name)
 		os.rename(old_name, new_name)
-	print("now", os.getcwd())
+
+	print("The folder is now called", os.getcwd())
 		
-
-def move():
-	SCRIPT_LOCATION = os.getcwd() # The path to the directory of containing this code
-	#BASENAME = os.path.basename( path_to_dir ) # The name of the directory containing this code
-	#print("Move", path_to_dir, BASENAME)
-	print("Moving", SCRIPT_LOCATION, "to", HOME)
-	shutil.move(SCRIPT_LOCATION, HOME)
-
 
 def isHiddenFile(filename):
 	return filename.startswith('.')
 
 def removeContent(path_of_content):
+	'''
+	Calls the appropriate commands to remove a file, whether it is a directory or a file
+	'''
 	if( os.path.isfile( path_of_content ) ): # if content is a file
 		os.remove(path_of_content)
 
@@ -40,10 +44,14 @@ def removeContent(path_of_content):
 		shutil.rmtree(path_of_content) # Delete the entire folder, including contents
 
 	else:
-		print("Will not remove", path_of_content)
+		print("Will now remove", path_of_content)
 
-def main():
-	print("Phase 1")
+def removeBaseDirectoryFiles():
+	'''
+	Removes all subdirectories and/or files within each Base Directory
+	'''
+	os.chdir(HOME)
+
 	# First, remove all files inside each of the base directories, not including Home
 	for directory in BASE_DIRECTORIES:
 		path_of_base_dir = os.path.join(HOME, directory)
@@ -52,10 +60,11 @@ def main():
 		for content in os.listdir():
 			path_of_content = os.path.join(os.getcwd(), content)
 			removeContent(path_of_content)
-
-
-	# Removing all files within Home
-	print("Phase 2")
+	
+def removeHomeFiles():
+	'''
+	Removes all files within the Home Directory, not including the any of the Base Directories
+	'''
 	os.chdir(HOME)
 
 	# Iterate through contents of Home
@@ -66,20 +75,39 @@ def main():
 			print("REMOVING", path_of_content)
 			removeContent(path_of_content)
 
+def removeOldScript():
+	old_alias_file = os.path.join(HOME, '.bash_aliases')
+	old_directory = os.path.join(HOME, '.127pythonscript')
+
+	if( os.path.isfile( old_alias_file ) ):
+		print("Removing old alias file")
+		removeContent( old_alias_file )
+	if( os.path.isdir( old_directory ) ):
+		print("Removing old script directory")
+		removeContent( old_directory )
+	
+
+def updateScript():
+	old_loc = os.getcwd()
+	os.chdir(SCRIPT_LOCATION)
+	print("Checking for updates from GitHub")
+	os.system('git pull origin master')
+	os.chdir(old_loc)
+
+def main():
+	removeBaseDirectoryFiles()
+	removeHomeFiles()
 	print("Finished Cleaning")
+
+	# Removes the old version of the script
+	removeOldScript()
+	# Check for updates at the end of every execution
+	updateScript()
 
 
 if __name__ == "__main__":
-	FLAG = None
+	if( isFolderVisible() ):
+		hide()
+	main()
 
-	if(len(SCRIPT_PARAMS) > 0 ):
-		FLAG = SCRIPT_PARAMS[0]
 
-		if(FLAG == "hide"):
-			hide()
-		
-		elif(FLAG == "move"):
-			move()
-	else:
-		pass
-		#main()
